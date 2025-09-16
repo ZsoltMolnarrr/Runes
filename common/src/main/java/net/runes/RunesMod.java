a7bd801acf888a66baf3cfb2ce5fbf6a6dfe7329
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-09-16 16:41:21 +0200

    Fix pouch registration for neo

--- common/src/main/java/net/runes/RunesMod.java
+++ common/src/main/java/net/runes/RunesMod.java
@@ -13,15 +13,9 @@ public class RunesMod {
     public static final String ID = "runes";
 
     public static void init() {
-
-
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> {
             content.add(RuneCraftingBlock.ITEM);
         });
-
-        if (FabricLoader.getInstance().isModLoaded("bundleapi")) {
-            RunePouches.register();
-        }
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
             for(var entry: RuneItems.entries) {
                 content.add(entry.item());
@@ -49,6 +43,9 @@ public class RunesMod {
         Registry.register(Registries.ITEM, Identifier.of(ID, RuneCraftingBlock.NAME), RuneCraftingBlock.ITEM);
         for(var entry: RuneItems.entries) {
             Registry.register(Registries.ITEM, entry.id(), entry.item());
+        }
+        if (FabricLoader.getInstance().isModLoaded("bundleapi")) {
+            RunePouches.register();
         }
     }
 }
