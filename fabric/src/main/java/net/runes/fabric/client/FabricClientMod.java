a6153fbde31919495ae05049b9cd7dda37f01e87
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-09-18 21:53:31 +0200

    Neo native block render type

--- fabric/src/main/java/net/runes/fabric/client/FabricClientMod.java
+++ fabric/src/main/java/net/runes/fabric/client/FabricClientMod.java
@@ -1,11 +1,17 @@
 package net.runes.fabric.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 import net.runes.client.RunesClientMod;
+import net.runes.crafting.RuneCraftingBlock;
 
 public final class FabricClientMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         RunesClientMod.init();
+
+        // Fabric-specific render layer registration
+        BlockRenderLayerMap.INSTANCE.putBlock(RuneCraftingBlock.INSTANCE, RenderLayer.getCutout());
     }
 }
